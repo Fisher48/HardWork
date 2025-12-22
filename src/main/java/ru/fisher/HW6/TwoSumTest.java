29e8cefebe51d1d945cee09a0c82fff5bde38c11
Fisher48 <rybakov48@yandex.ru>
2025-12-22 10:42:15 +0300

    Changed realization after failed test

--- src/main/java/ru/fisher/HW6/TwoSumTest.java
+++ src/main/java/ru/fisher/HW6/TwoSumTest.java
@@ -10,28 +10,44 @@ public class TwoSumTest {
     void simpleTest() {
         int[] array = new int[]{5, 8};
         int target = 13;
-        assertArrayEquals(new int[]{5, 8}, TwoSum.twoSum(array, target));
+        assertArrayEquals(new int[]{0, 1}, TwoSum.twoSum(array, target));
     }
 
     @Test
     void twoSumWithThreeElementsTest() {
         int[] array = new int[]{1, 12, 5};
         int target = 6;
-        assertArrayEquals(new int[]{1, 5}, TwoSum.twoSum(array, target));
+        assertArrayEquals(new int[]{0, 2}, TwoSum.twoSum(array, target));
     }
 
     @Test
     void twoSumWithFourElementsTest() {
         int[] array = new int[]{2, 7, 11, 15};
         int target = 9;
-        assertArrayEquals(new int[]{2, 7}, TwoSum.twoSum(array, target));
+        assertArrayEquals(new int[]{0, 1}, TwoSum.twoSum(array, target));
     }
 
     @Test
     void twoSumWithThreeElementsFromExampleTest() {
         int[] array = new int[]{3, 2, 4};
         int target = 6;
-        assertArrayEquals(new int[]{2, 4}, TwoSum.twoSum(array, target));
+        assertArrayEquals(new int[]{1, 2}, TwoSum.twoSum(array, target));
     }
+
+    @Test
+    void twoSumWithTwoSameElementsTest() {
+        int[] array = new int[]{3, 3};
+        int target = 6;
+        assertArrayEquals(new int[]{0, 1}, TwoSum.twoSum(array, target));
+    }
+
+    @Test
+    void twoSumTest() {
+        int[] array = new int[]{2, 5, 5, 11};
+        int target = 10;
+        assertArrayEquals(new int[]{1, 2}, TwoSum.twoSum(array, target));
+    }
+
+
 
 }
